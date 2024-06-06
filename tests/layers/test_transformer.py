ff5244c0586f0cca4e1b02827dd490f14b37384d
apehex <17317183+apehex@users.noreply.github.com>
2024-06-06 12:21:34 +0200

    Move the FFN gate to the transformer layers

--- tests/layers/test_transformer.py
+++ tests/layers/test_transformer.py
@@ -4,6 +4,40 @@ import tensorflow as tf
 import mlable.layers.transformer
 import mlable.utils
 
+# FF ##########################################################################
+
+class FeedForwardBlockTest(tf.test.TestCase):
+    def setUp(self):
+        super(FeedForwardBlockTest, self).setUp()
+        self._test_cases = [
+            {
+                'input_dim': 2,
+                'hidden_dim': 3,
+                'batch_size': 2,
+                'expected_val': [11.726998, 47.998482],
+                'expected_shape': (2, 1, 2),},]
+
+    def test_ffn(self):
+        for __case in self._test_cases:
+            # inputs
+            __inputs = tf.reshape(tf.range(1, __case['batch_size'] + 1, dtype=tf.float32), (__case['batch_size'], 1, 1))
+            __inputs = tf.repeat(__inputs, __case['input_dim'], axis=-1)
+            # init
+            __layer = mlable.layers.transformer.FeedForwardGate(
+                input_dim=__case["input_dim"],
+                hidden_dim=__case["hidden_dim"])
+            # build
+            _ = __layer(__inputs)
+            # set weights
+            __layer._gelu.set_weights([np.ones((__case['input_dim'], __case['hidden_dim']))])
+            __layer._linear.set_weights([np.ones((__case['input_dim'], __case['hidden_dim']))])
+            __layer._output.set_weights([np.ones((__case['hidden_dim'], __case['input_dim']))])
+            # compute
+            __output = __layer(__inputs)
+            # test
+            np.testing.assert_array_almost_equal(__output[:, 0, 0].numpy().tolist(), __case["expected_val"])
+            self.assertEqual(tuple(__output.shape), __case['expected_shape'])
+
 # CACHED ATTENTION ############################################################
 
 class CachedAttentionTest(tf.test.TestCase):
