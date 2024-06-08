2837f8dc0f5cb69f16677e426f434b1f22d6d3ed
apehex <apehex@protonmail.com>
2024-06-08 21:24:10 +0200

    Add tests + fix errors in sampling mode

--- tests/layers/test_transformer.py
+++ tests/layers/test_transformer.py
@@ -33,60 +33,86 @@ class FeedForwardBlockTest(tf.test.TestCase):
             __layer._linear.set_weights([np.ones((__case['input_dim'], __case['hidden_dim']))])
             __layer._output.set_weights([np.ones((__case['hidden_dim'], __case['input_dim']))])
             # compute
-            __output = __layer(__inputs)
+            __outputs = __layer(__inputs)
             # test
-            np.testing.assert_array_almost_equal(__output[:, 0, 0].numpy().tolist(), __case["expected_val"])
-            self.assertEqual(tuple(__output.shape), __case['expected_shape'])
+            np.testing.assert_array_almost_equal(__outputs[:, 0, 0].numpy().tolist(), __case["expected_val"])
+            self.assertEqual(tuple(__outputs.shape), __case['expected_shape'])
 
 # CACHED ATTENTION ############################################################
 
 class CachedAttentionTest(tf.test.TestCase):
 
-    def test_masked_attention(self):
-        """Test with a mask tensor."""
-        __batch_dim, __seq_dim, __num_heads, __head_dim = 3, 4, 2, 2
-        # GPU/CPU case.
-        __init_dim = 0
-        # Directly tests the keras layer.
-        __cache = mlable.utils.create_cache(__batch_dim, __init_dim, __num_heads, __head_dim)
+    def test_batch_decode(self):
+        __batch_dim, __seq_dim, __embed_dim, __num_heads, __head_dim = 2, 4, 6, 2, 3
+        # training mode
+        __cache = None
+        __mask = None
+        __step = None
         # basic attention
         __layer = mlable.layers.transformer.CachedMultiHeadAttention(num_heads=__num_heads, key_dim=__head_dim)
         # input data
-        __from_inputs = tf.zeros((__batch_dim, __seq_dim, 8), dtype=np.float32)
-        # random mask
-        __mask = np.random.randint(2, size=(__batch_dim, __seq_dim, __seq_dim))
+        __inputs = tf.zeros((__batch_dim, __seq_dim, __embed_dim), dtype=np.float32)
         # call
-        __output, __cache = __layer(query=__from_inputs, value=__from_inputs, cache=__cache, attention_mask=__mask)
-        # checks
-        self.assertEqual(__output.shape, (3, 4, 8))
-        self.assertEqual(__cache.shape, (2, 3, 4, 2, 2))
-        # without cache
-        __output, __cache = __layer(query=__from_inputs, value=__from_inputs, attention_mask=__mask)
-        self.assertEqual(__output.shape, (3, 4, 8))
-        self.assertEqual(__cache.shape, (2, 3, 4, 2, 2))
+        __outputs, __scores, __cache = __layer(query=__inputs, value=__inputs, cache=__cache, attention_mask=__mask, step=__step, return_attention_scores=True)
+        # check
+        self.assertEqual(__outputs.shape, __inputs.shape)
+        self.assertEqual(__scores.shape, (__batch_dim, __num_heads, __seq_dim, __seq_dim))
+        self.assertEqual(__cache, None)
 
-    def test_padded_decode(self):
-        """Test with a mask tensor."""
-        __num_heads, __head_dim, __batch_dim, __seq_dim = 2, 2, 3, 4
-        # GPU/CPU case.
-        __init_dim = __seq_dim
-        # Directly tests the keras layer.
-        __cache = mlable.utils.create_cache(__batch_dim, __init_dim, __num_heads, __head_dim)
+    def test_sequential_decode_update(self):
+        __batch_dim, __seq_dim, __embed_dim, __num_heads, __head_dim = 1, 4, 6, 2, 3
+        # sampling mode
+        __cache = mlable.utils.create_cache(batch_dim=__batch_dim, cache_dim=__seq_dim, num_heads=__num_heads, head_dim=__head_dim)
+        # random mask
+        __mask = np.random.randint(2, size=(__batch_dim, 1, __seq_dim))
+        # update index
+        __step = __seq_dim // 2
         # basic attention
         __layer = mlable.layers.transformer.CachedMultiHeadAttention(num_heads=__num_heads, key_dim=__head_dim)
-        # Generate data for the input (non-mask) tensors.
-        __from_inputs = tf.zeros((__batch_dim, __seq_dim, 8), dtype=np.float32)
-        # decode step
-        __step = 2
-        # random mask
-        __mask = np.random.randint(2, size=(__batch_dim, __seq_dim, __seq_dim), dtype=np.int32)
+        # input data
+        __inputs = tf.ones((__batch_dim, 1, __embed_dim), dtype=np.float32)
         # call
-        __output, __cache = __layer(
-                query=__from_inputs,
-                value=__from_inputs,
-                attention_mask=__mask,
-                cache=__cache,
-                step=__step)
-        # checks
-        self.assertEqual(__output.shape, (3, 4, 8))
-        self.assertEqual(__cache.shape, (2, 3, 4, 2, 2))
+        __outputs, __scores, __cache = __layer(query=__inputs, value=__inputs, cache=__cache, attention_mask=__mask, step=__step, return_attention_scores=True)
+        # check
+        self.assertEqual(__outputs.shape, __inputs.shape)
+        self.assertEqual(__scores.shape, (__batch_dim, __num_heads, 1, __seq_dim))
+        self.assertEqual(__cache.shape, (2, __batch_dim, __seq_dim, __num_heads, __head_dim))
+
+    def test_sequential_decode_append(self):
+        __batch_dim, __seq_dim, __embed_dim, __num_heads, __head_dim = 1, 4, 6, 2, 3
+        # sampling mode
+        __cache = mlable.utils.create_cache(batch_dim=__batch_dim, cache_dim=__seq_dim, num_heads=__num_heads, head_dim=__head_dim)
+        # random mask
+        __mask = np.random.randint(2, size=(__batch_dim, 1, __seq_dim + 1))
+        # update index
+        __step = None
+        # basic attention
+        __layer = mlable.layers.transformer.CachedMultiHeadAttention(num_heads=__num_heads, key_dim=__head_dim)
+        # input data
+        __inputs = tf.ones((__batch_dim, 1, __embed_dim), dtype=np.float32)
+        # call
+        __outputs, __scores, __cache = __layer(query=__inputs, value=__inputs, cache=__cache, attention_mask=__mask, step=__step, return_attention_scores=True)
+        # check
+        self.assertEqual(__outputs.shape, __inputs.shape)
+        self.assertEqual(__scores.shape, (__batch_dim, __num_heads, 1, __seq_dim + 1))
+        self.assertEqual(__cache.shape, (2, __batch_dim, __seq_dim + 1, __num_heads, __head_dim))
+
+    # TODO test mask with cache appending
+    def test_random_masking(self):
+        __batch_dim, __seq_dim, __embed_dim, __num_heads, __head_dim = 1, 4, 6, 2, 3
+        # sampling mode
+        __cache = mlable.utils.create_cache(batch_dim=__batch_dim, cache_dim=__seq_dim, num_heads=__num_heads, head_dim=__head_dim)
+        # random mask
+        __mask = np.random.randint(2, size=(__batch_dim, __seq_dim, __seq_dim))
+        # update index
+        __step = __seq_dim // 2
+        # basic attention
+        __layer = mlable.layers.transformer.CachedMultiHeadAttention(num_heads=__num_heads, key_dim=__head_dim)
+        # input data
+        __inputs = tf.ones((__batch_dim, 1, __embed_dim), dtype=np.float32)
+        # call
+        __outputs, __scores, __cache = __layer(query=__inputs, value=__inputs, cache=__cache, attention_mask=__mask, step=__step, return_attention_scores=True)
+        # check
+        self.assertEqual(__outputs.shape, (__batch_dim, __seq_dim, __embed_dim))
+        self.assertEqual(__scores.shape, (__batch_dim, __num_heads, __seq_dim, __seq_dim))
+        self.assertEqual(__cache.shape, (2, __batch_dim, __seq_dim, __num_heads, __head_dim))
