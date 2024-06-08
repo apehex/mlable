6b6217fb49e4dfcbb8fb343284a1d73175f11bb3
apehex <apehex@protonmail.com>
2024-06-08 21:22:57 +0200

    Reuse the parent methods when the process is the same as MultiHeadAttention

--- mlable/layers/transformer.py
+++ mlable/layers/transformer.py
@@ -62,12 +62,14 @@ class CachedMultiHeadAttention(tf.keras.layers.MultiHeadAttention):
         attention_mask: tf.Tensor=None,
         return_attention_scores: bool=False,
         use_causal_mask: bool=True,
+        training: bool=None,
     ) -> tf.Tensor:
         if (hasattr(self, "_build_from_signature") and hasattr(self, "_built_from_signature") and not self._built_from_signature):
             self._build_from_signature(query=query, value=value, key=key)
         # attention mask
         __mask = self._compute_attention_mask(query=query, value=value, attention_mask=attention_mask, use_causal_mask=use_causal_mask) # TODO here or after the cache update??
         # init
+        __cache = None
         __key = value if key is None else key
         # [B, F, N ,H]
         __query = self._query_dense(query)
@@ -79,18 +81,9 @@ class CachedMultiHeadAttention(tf.keras.layers.MultiHeadAttention):
         if cache is not None:
             __key = mlable.utils.update_cache(tensor=__key, cache=cache[0], step=step, axis=self._attention_axes[0]) # custom seq axis?
             __value = mlable.utils.update_cache(tensor=__value, cache=cache[1], step=step, axis=self._attention_axes[0]) # custom seq axis?
-        # if cache was none it contains only the current values
-        __cache = tf.stack(values=(__key, __value), axis=0)
-        # rescale
-        __query = tf.multiply(__query, 1.0 / math.sqrt(float(self._key_dim)))
-        # attention scores
-        __scores = tf.einsum(self._dot_product_equation, __key, __query)
-        # [B, N, F, T]
-        __scores = self._masked_softmax(__scores, __mask)
-        # dropout
-        __scores = self._dropout_layer(__scores)
-        # [B, F, N, H]
-        __output = tf.einsum(self._combine_equation, __scores, __value)
+            __cache = tf.stack(values=(__key, __value), axis=0)
+        # use the parent functionalities
+        __output, __scores = self._compute_attention(query=__query, key=__key, value=__value, attention_mask=__mask, training=training)
         # projection
         __output = self._output_dense(__output)
         # output
