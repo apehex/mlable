d1c37813861cd6592e3da0e732dec0fa5042975a
apehex <17317183+apehex@users.noreply.github.com>
2024-07-04 14:25:26 +0200

    Transform a function into a mapping

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -25,6 +25,21 @@ class ComposeTest(tf.test.TestCase):
         self.assertEqual([__h(__e) for __e in self._random], [__hi2(__e) for __e in self._random])
         self.assertEqual(self._random, __f4(self._random))
 
+# FN MAP ######################################################################
+
+class DistributeTest(tf.test.TestCase):
+    def setUp(self):
+        super(DistributeTest, self).setUp()
+        self._random = [random.uniform(-8., 8.) for _ in range(32)]
+
+    def test_values(self):
+        __f = lambda __x: __x ** 2
+        __g = lambda __x: -__x
+        __fn = mlable.utils.distribute(__f)
+        __gn = mlable.utils.distribute(__g)
+        self.assertEqual([(__e ** 2, __e ** 2, __e ** 2) for __e in self._random], [__fn(__e, __e, __e) for __e in self._random])
+        self.assertEqual([(-__e,) for __e in self._random], [__gn(__e) for __e in self._random])
+
 # CACHE #######################################################################
 
 class CacheToolingTest(tf.test.TestCase):
