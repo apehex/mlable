7c8740ad5f6c11a84208c1fcc9229954ad7be612
apehex <17317183+apehex@users.noreply.github.com>
2024-08-25 21:19:47 +0200

    Add an accuracy metric for raw value predictions

--- mlable/metrics.py
+++ mlable/metrics.py
@@ -78,3 +78,39 @@ class BinaryGroupAccuracy(tf.keras.metrics.MeanMetricWrapper):
         __config = super(BinaryGroupAccuracy, self).get_config()
         __config.update(self._config)
         return __config
+
+# BINARY ######################################################################
+
+@ks.saving.register_keras_serializable(package='metrics', name='raw_group_accuracy')
+def raw_group_accuracy(y_true: tf.Tensor, y_pred: tf.Tensor, group: int=1, factor: float=256.0, dtype: tf.dtypes.DType=None) -> tf.Tensor:
+    __dtype = dtype or y_true.dtype
+    # category indexes
+    __yt = mlable.sampling.raw(prediction=y_true, factor=factor, random=False)
+    __yp = mlable.sampling.raw(prediction=y_pred, factor=factor, random=False)
+    # matching
+    __match = tf.equal(__yt, __yp)
+    # group all the predictions for a given token
+    if group and group > 1:
+        # repeat values so that the reduced tensor has the same shape as the original
+        __match = mlable.ops.reduce_all(data=__match, group=group, axis=-1, keepdims=True)
+    # mean over sequence axis
+    return tf.cast(__match, dtype=__dtype)
+
+@ks.saving.register_keras_serializable(package='metrics')
+class RawGroupAccuracy(tf.keras.metrics.MeanMetricWrapper):
+    def __init__(self, group: int=1, factor: float=256.0, name: str='raw_group_accuracy', dtype: tf.dtypes.DType=None, **kwargs):
+        # serialization wrapper
+        __wrap = ks.saving.register_keras_serializable(package='metrics', name='raw_group_accuracy')
+        # adapt the measure
+        __fn = __wrap(functools.partial(raw_group_accuracy, group=group, factor=factor, dtype=dtype))
+        # init
+        super(RawGroupAccuracy, self).__init__(fn=__fn, name=name, dtype=dtype, **kwargs)
+        # config
+        self._config = {'group': group, 'factor': factor}
+        # sould be maximized
+        self._direction = 'up'
+
+    def get_config(self) -> dict:
+        __config = super(RawGroupAccuracy, self).get_config()
+        __config.update(self._config)
+        return __config
