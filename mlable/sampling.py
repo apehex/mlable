6f2317aa439ed48644bfeed7c41e802ebdc8f958
apehex <apehex@protonmail.com>
2024-08-28 17:24:10 +0200

    Move the factoring of the predictions from metrics to sampling + add tests

--- mlable/sampling.py
+++ mlable/sampling.py
@@ -4,18 +4,23 @@ import mlable.ops
 
 # BINARY ###############################################################
 
-def binary(prediction: tf.Tensor, threshold: float=0.5, random: bool=False) -> tf.Tensor:
+def binary(prediction: tf.Tensor, depth: int=-1, threshold: float=0.5, random: bool=False) -> tf.Tensor:
     # meta
     __threshold = tf.cast(threshold, prediction.dtype)
+    # group the bits of each encoded value
+    __prediction = prediction if (depth < 2) else mlable.ops.divide(prediction, input_axis=-2, output_axis=-1, factor=depth, insert=True)
     # binary tensor
-    __bits = tf.cast(prediction > __threshold, dtype=tf.int32)
+    __bits = tf.cast(__prediction > __threshold, dtype=tf.int32)
     # expand to match the input rank
     return mlable.ops._reduce_base(data=__bits, base=2, axis=-1, keepdims=False)
 
 # CATEGORICAL #################################################################
 
-def categorical(prediction: tf.Tensor, random: bool=False) -> tf.Tensor:
-    return tf.argmax(input=prediction, axis=-1, output_type=tf.int32)
+def categorical(prediction: tf.Tensor, depth: int=-1, random: bool=False) -> tf.Tensor:
+    # isolate each one-hot vector
+    __prediction = prediction if (depth < 2) else mlable.ops.divide(prediction, input_axis=-2, output_axis=-1, factor=depth, insert=True)
+    # return the index with the highest probability
+    return tf.argmax(input=__prediction, axis=-1, output_type=tf.int32)
 
 # RAW #########################################################################
 
