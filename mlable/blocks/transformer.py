173ad9591ac998b1427e7292457f5aa9e2675303
apehex <17317183+apehex@users.noreply.github.com>
2024-10-11 11:32:39 +0200

    Make the *building method compatible with tf v2.15

--- mlable/blocks/transformer.py
+++ mlable/blocks/transformer.py
@@ -136,8 +136,10 @@ class BaseCrossAttentionBlock(BaseAttentionBlock):
         # layers
         self._key_norm = tf.keras.layers.LayerNormalization(axis=-1, epsilon=epsilon, center=center, scale=scale) # rms_scaling=True
         self._value_norm = tf.keras.layers.LayerNormalization(axis=-1, epsilon=epsilon, center=center, scale=scale) # rms_scaling=True
+        # specific building mechanism != built-in
+        self._built = False
 
-    def build(self, query_shape: tf.TensorShape, key_shape: tf.TensorShape, value_shape: tf.TensorShape) -> None:
+    def _build(self, query_shape: tf.TensorShape, key_shape: tf.TensorShape, value_shape: tf.TensorShape) -> None:
         # the input shape is progated / unchanged
         self._norm.build(query_shape)
         self._key_norm.build(key_shape)
@@ -150,10 +152,18 @@ class BaseCrossAttentionBlock(BaseAttentionBlock):
             self._attention.build(query_shape=query_shape, key_shape=key_shape, value_shape=value_shape)
         # register
         self.built = True
+        self._built = True
+
+    def build(self, query_shape: tf.TensorShape, key_shape: tf.TensorShape=None, value_shape: tf.TensorShape=None) -> None:
+        if (key_shape is not None) and (value_shape is not None):
+            self._build(query_shape=query_shape, key_shape=key_shape, value_shape=value_shape)
 
 @tf.keras.utils.register_keras_serializable(package='blocks')
 class CrossAttentionBlock(BaseCrossAttentionBlock):
     def call(self, query: tf.Tensor, key: tf.Tensor, value: tf.Tensor, **kwargs) -> tf.Tensor:
+        # build
+        if not self._built:
+            self._build(query_shape=tuple(query.shape), key_shape=tuple(key.shape), value_shape=tuple(value.shape))
         # normalize
         __q = self._norm(query)
         __k = self._key_norm(key)
@@ -320,15 +330,26 @@ class CrossDecoderBlock(tf.keras.layers.Layer):
         # layers
         self._attention = CrossAttentionBlock(head_num=head_num, head_dim=head_dim, sequence_axis=sequence_axis, epsilon=epsilon, bias=bias, center=center, scale=scale)
         self._ffn = FeedForwardBlock(embed_dim=embed_dim, hidden_dim=hidden_dim, epsilon=epsilon, center=center, scale=scale)
+        # specific building mechanism != built-in
+        self._built = False
 
-    def build(self, query_shape: tf.TensorShape, key_shape: tf.TensorShape, value_shape: tf.TensorShape) -> None:
+    def _build(self, query_shape: tf.TensorShape, key_shape: tf.TensorShape, value_shape: tf.TensorShape) -> None:
         # the input shape is propagated / unchanged
-        self._attention.build(query_shape=query_shape, key_shape=key_shape, value_shape=value_shape)
+        self._attention._build(query_shape=query_shape, key_shape=key_shape, value_shape=value_shape)
         self._ffn.build(query_shape)
         # register
         self.built = True
+        self._built = True
+
+    def build(self, query_shape: tf.TensorShape, key_shape: tf.TensorShape=None, value_shape: tf.TensorShape=None) -> None:
+        if (key_shape is not None) and (value_shape is not None):
+            self._build(query_shape=query_shape, key_shape=key_shape, value_shape=value_shape)
 
     def call(self, query: tf.Tensor, key: tf.Tensor, value: tf.Tensor, **kwargs) -> tf.Tensor:
+        # build
+        if not self._built:
+            self._build(query_shape=tuple(query.shape), key_shape=tuple(key.shape), value_shape=tuple(value.shape))
+        # forward
         return self._ffn(self._attention(query=query, key=key, value=value, **kwargs))
 
     def get_config(self) -> dict:
@@ -343,6 +364,9 @@ class CrossDecoderBlock(tf.keras.layers.Layer):
 @tf.keras.utils.register_keras_serializable(package='blocks')
 class ResidualCrossDecoderBlock(CrossDecoderBlock):
     def call(self, query: tf.Tensor, key: tf.Tensor, value: tf.Tensor, **kwargs) -> tf.Tensor:
+        # build
+        if not self._built:
+            self._build(query_shape=tuple(query.shape), key_shape=tuple(key.shape), value_shape=tuple(value.shape))
         # residual + cross attention
         __x = query + self._attention(query=query, key=key, value=value, **kwargs)
         # residual + augmentation
