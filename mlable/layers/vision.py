ef2b8c80cac8e7d47af8cda6036dc66e48aafdf3
apehex <apehex@protonmail.com>
2024-10-16 11:10:16 +0200

    Improve the patching layer to handle any tensor of rank >= 2

--- mlable/layers/vision.py
+++ mlable/layers/vision.py
@@ -2,6 +2,7 @@ import math
 
 import tensorflow as tf
 
+import mlable.layers.reshaping
 import mlable.shaping
 
 # CONSTANTS ####################################################################
@@ -11,22 +12,56 @@ EPSILON = 1e-6
 # IMAGE PATCH EXTRACTION #######################################################
 
 class Patching(tf.keras.layers.Layer):
-    def __init__(self, width: int, height: int, **kwargs):
+    def __init__(
+        self,
+        height_dim: int,
+        width_dim: int,
+        height_axis: int=1,
+        width_axis: int=2,
+        merge_patch_axes: bool=True,
+        merge_space_axes: bool=True,
+        **kwargs
+    ) -> None:
         # init
         super(Patching, self).__init__(**kwargs)
+        # always interpret the smallest axis as height
+        __height_axis = min(height_axis, width_axis)
+        __width_axis = max(height_axis, width_axis)
+        __height_dim = height_dim if height_axis < width_axis else width_dim
+        __width_dim = width_dim if height_axis < width_axis else height_dim
         # save for import / export
-        self._config = {'width': width, 'height': height,}
+        self._config = {
+            'height_axis': __height_axis,
+            'height_dim': __height_dim,
+            'width_axis': __width_axis,
+            'width_dim': __width_dim,
+            'merge_patch_axes': merge_patch_axes,
+            'merge_space_axes': merge_space_axes,}
+        # reshaping layers
+        self._split_height = mlable.layers.reshaping.Divide(input_axis=__height_axis, output_axis=__height_axis + 1, factor=__height_dim, insert=True)
+        self._split_width = mlable.layers.reshaping.Divide(input_axis=__width_axis, output_axis=__width_axis + 1, factor=__width_dim, insert=True)
+        self._merge_space = mlable.layers.reshaping.Merge(left_axis=__height_axis, right_axis=__height_axis + 1, left=True)
+        self._merge_patch = mlable.layers.reshaping.Merge(left_axis=__width_axis + 1, right_axis=__width_axis + 2, left=True) # moved by splitting axes
 
     def build(self, input_shape: tf.TensorShape=None) -> None:
         self.built = True
 
     def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
-        return tf.image.extract_patches(
-            images=inputs,
-            sizes=[1, self._config['height'], self._config['width'], 1],
-            strides=[1, self._config['height'], self._config['width'], 1],
-            rates=[1, 1, 1, 1],
-            padding='VALID')
+        # parse the input shape
+        __axis_h, __axis_w = self._config['height_axis'], self._config['width_axis']
+        # split the last axis first, because it increases the position of the following axes
+        __patched = self._split_height(self._split_width(inputs))
+        # the width axis has been pushed right by the insertion of the patch height axis
+        __perm = mlable.shaping.swap_axes(rank=len(list(__patched.shape)), left=__axis_h + 1, right=__axis_w + 1)
+        # group the space axes and the patch axes
+        __patched = tf.transpose(__patched, perm=__perm, conjugate=False)
+        # merge the last axes first because it moves other axes
+        if self._config['merge_patch_axes']:
+            __patched = self._merge_patch(__patched)
+        if self._config['merge_space_axes']:
+            __patched = self._merge_space(__patched)
+        # donzo
+        return __patched
 
     def get_config(self) -> dict:
         __config = super(RotaryPositionalEmbedding, self).get_config()
@@ -40,7 +75,14 @@ class Patching(tf.keras.layers.Layer):
 # RECOMPOSE THE IMAGE #########################################################
 
 class Unpatching(tf.keras.layers.Layer):
-    def __init__(self, width: int, height: int, **kwargs):
+    def __init__(
+        self,
+        width_dim: int,
+        height_dim: int,
+        space_axes: iter=[1, 2],
+        patch_axes: iter=[3, 4],
+        **kwargs
+    ) -> None:
         # init
         super(Unpatching, self).__init__(**kwargs)
         # save for import / export
@@ -55,11 +97,11 @@ class Unpatching(tf.keras.layers.Layer):
         # patch channels => pixel channels
         __channel_dim = __channel_dim // (self._config['width'] * self._config['height'])
         # split the patch channels into individual pixels
-        __patches = tf.reshape(inputs, shape=(__batch_dim, __height_num, __width_num, self._config['height'], self._config['width'], __channel_dim))
+        __patched = tf.reshape(inputs, shape=(__batch_dim, __height_num, __width_num, self._config['height'], self._config['width'], __channel_dim))
         # move the patch axes next to the corresponding image axes
-        __patches = tf.transpose(__patches, perm=(0, 1, 3, 2, 4, 5), conjugate=False)
+        __patched = tf.transpose(__patched, perm=(0, 1, 3, 2, 4, 5), conjugate=False)
         # merge the patch and image axes
-        return tf.reshape(__patches, shape=(__batch_dim, __height_num * self._config['height'], __width_num * self._config['width'], __channel_dim))
+        return tf.reshape(__patched, shape=(__batch_dim, __height_num * self._config['height'], __width_num * self._config['width'], __channel_dim))
 
     def get_config(self) -> dict:
         __config = super(RotaryPositionalEmbedding, self).get_config()
---
+++ tests/layers/test_vision.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+
+import mlable.layers.vision
+
+# BOTH #########################################################################
+
+class PatchingTest(tf.test.TestCase):
+    def setUp(self):
+        super(PatchingTest, self).setUp()
+        self._test_cases = [
+            {
+                'init': {
+                    'height_axis': 0,
+                    'height_dim': 3,
+                    'width_axis': 1,
+                    'width_dim': 2,
+                    'merge_patch_axes': False,
+                    'merge_space_axes': False,},
+                'input': {
+                    'inputs': tf.reshape(tf.range(48, dtype=tf.int32), shape=(6, 8)),},
+                'output': {
+                    'shape': (2, 4, 3, 2),
+                    'values': tf.convert_to_tensor(
+                        [
+                            [
+                                [[0, 1], [8, 9], [16, 17]],
+                                [[2, 3], [10, 11], [18, 19]],
+                                [[4, 5], [12, 13], [20, 21]],
+                                [[6, 7], [14, 15], [22, 23]],],
+                            [
+                                [[24, 25], [32, 33], [40, 41]],
+                                [[26, 27], [34, 35], [42, 43]],
+                                [[28, 29], [36, 37], [44, 45]],
+                                [[30, 31], [38, 39], [46, 47]],],])}},
+            {
+                'init': {
+                    'height_axis': 0,
+                    'height_dim': 3,
+                    'width_axis': 1,
+                    'width_dim': 2,
+                    'merge_patch_axes': True,
+                    'merge_space_axes': True,},
+                'input': {
+                    'inputs': tf.reshape(tf.range(48, dtype=tf.int32), shape=(6, 8)),},
+                'output': {
+                    'shape': (8, 6),
+                    'values': tf.convert_to_tensor(
+                        [
+                            [0, 1, 8, 9, 16, 17],
+                            [2, 3, 10, 11, 18, 19],
+                            [4, 5, 12, 13, 20, 21],
+                            [6, 7, 14, 15, 22, 23],
+                            [24, 25, 32, 33, 40, 41],
+                            [26, 27, 34, 35, 42, 43],
+                            [28, 29, 36, 37, 44, 45],
+                            [30, 31, 38, 39, 46, 47],])}},
+            {
+                'init': {
+                    'height_axis': 1,
+                    'height_dim': 2,
+                    'width_axis': 0,
+                    'width_dim': 3,
+                    'merge_patch_axes': True,
+                    'merge_space_axes': True,},
+                'input': {
+                    'inputs': tf.reshape(tf.stack([tf.range(48, dtype=tf.int32), tf.range(48, dtype=tf.int32)], axis=-1), shape=(6, 8, 2)),},
+                'output': {
+                    'shape': (8, 6, 2),
+                    'values': tf.convert_to_tensor(
+                        [
+                            [[0, 0], [1, 1], [8, 8], [9, 9], [16, 16], [17, 17]],
+                            [[2, 2], [3, 3], [10, 10], [11, 11], [18, 18], [19, 19]],
+                            [[4, 4], [5, 5], [12, 12], [13, 13], [20, 20], [21, 21]],
+                            [[6, 6], [7, 7], [14, 14], [15, 15], [22, 22], [23, 23]],
+                            [[24, 24], [25, 25], [32, 32], [33, 33], [40, 40], [41, 41]],
+                            [[26, 26], [27, 27], [34, 34], [35, 35], [42, 42], [43, 43]],
+                            [[28, 28], [29, 29], [36, 36], [37, 37], [44, 44], [45, 45]],
+                            [[30, 30], [31, 31], [38, 38], [39, 39], [46, 46], [47, 47]],])}},]
+
+    def test_patching(self):
+        for __case in self._test_cases:
+            __layer = mlable.layers.vision.Patching(**__case['init'])
+            __outputs = __layer(**__case['input'])
+            if 'shape' in __case['output']:
+                self.assertEqual(tuple(__outputs.shape), __case['output']['shape'])
+            if 'values' in __case['output']:
+                self.assertAllClose(__outputs, __case['output']['values'])
+
+# RECIPROCITY ##################################################################
+
+    # test width != height
+    # test axes permuted
+    # test channel first
+    # test axis in-between
+    # with / without merging
