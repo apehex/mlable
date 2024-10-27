68edce3df6b86e0add714af72dd21c7104b385ff
apehex <17317183+apehex@users.noreply.github.com>
2024-10-27 14:36:15 +0100

    Only update the sample weights and leverage the built-in loss function and reduction

--- mlable/models.py
+++ mlable/models.py
@@ -12,6 +12,13 @@ class ContrastModel(tf.keras.models.Model):
         y_pred: tf.Tensor=None,
         sample_weight: tf.Tensor=None,
     ):
-        __weights = mlable.masking.contrast(left=x, right=tf.cast(tf.argmax(y, axis=-1), dtype=x.dtype), weight=0.9)
-        __loss = super(ContrastModel, self).compute_loss(x, y, y_pred, sample_weight)
-        return __weights * __loss
+        # weight according to the difference between x and y (reduced)
+        __weights = mlable.masking.contrast(
+            left=x,
+            right=tf.cast(tf.argmax(y, axis=-1), dtype=x.dtype),
+            weight=0.9,
+            dtype=sample_weight.dtype)
+        # combine with the sample weights
+        __weights = __weights * sample_weight if (sample_weight is not None) else __weights
+        # apply the original loss and reduction of the model
+        return super(ContrastModel, self).compute_loss(x, y, y_pred, __weights)
