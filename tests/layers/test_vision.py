ad54b519c9e894d8e14e4b109487bd2e96fbb41c
apehex <apehex@protonmail.com>
2024-11-14 21:33:26 +0100

    Force the (un)patching inputs to have the space and patch axes split

--- tests/layers/test_vision.py
+++ tests/layers/test_vision.py
@@ -12,14 +12,12 @@ class PatchingTest(tf.test.TestCase):
                 'init': {
                     'height_axis': 0,
                     'width_axis': 1,
-                    'patch_dim': (3, 2),
-                    'merge_patch_axes': False,
-                    'merge_space_axes': False,},
+                    'patch_dim': (3, 2),},
                 'init_unpatching': {
-                    'space_dim': [6, 8],
-                    'patch_dim': [3, 2],
-                    'space_axes': [0, 1],
-                    'patch_axes': [2, 3],},
+                    'space_height_axis': 0,
+                    'space_width_axis': 1,
+                    'patch_height_axis': 2,
+                    'patch_width_axis': 3,},
                 'input': {
                     'inputs': tf.reshape(tf.range(48, dtype=tf.int32), shape=(6, 8)),},
                 'output': {
@@ -38,56 +36,30 @@ class PatchingTest(tf.test.TestCase):
                                 [[30, 31], [38, 39], [46, 47]],],])}},
             {
                 'init': {
-                    'height_axis': 0,
-                    'width_axis': 1,
-                    'patch_dim': (3, 2),
-                    'merge_patch_axes': True,
-                    'merge_space_axes': True,},
-                'init_unpatching': {
-                    'space_dim': (6, 8),
-                    'patch_dim': (3, 2),
-                    'space_axes': 0,
-                    'patch_axes': 1,},
-                'input': {
-                    'inputs': tf.reshape(tf.range(48, dtype=tf.int32), shape=(6, 8)),},
-                'output': {
-                    'shape': (8, 6),
-                    'values': tf.convert_to_tensor(
-                        [
-                            [0, 1, 8, 9, 16, 17],
-                            [2, 3, 10, 11, 18, 19],
-                            [4, 5, 12, 13, 20, 21],
-                            [6, 7, 14, 15, 22, 23],
-                            [24, 25, 32, 33, 40, 41],
-                            [26, 27, 34, 35, 42, 43],
-                            [28, 29, 36, 37, 44, 45],
-                            [30, 31, 38, 39, 46, 47],])}},
-            {
-                'init': {
                     'height_axis': 1,
                     'width_axis': 0,
-                    'patch_dim': (2, 3),
-                    'merge_patch_axes': True,
-                    'merge_space_axes': True,},
+                    'patch_dim': (2, 3),},
                 'init_unpatching': {
-                    'space_dim': (6, 8),
-                    'patch_dim': (3, 2),
-                    'space_axes': [0],
-                    'patch_axes': [1],},
+                    'space_height_axis': 0,
+                    'space_width_axis': 1,
+                    'patch_height_axis': 2,
+                    'patch_width_axis': 3,},
                 'input': {
                     'inputs': tf.reshape(tf.stack([tf.range(48, dtype=tf.int32), tf.range(48, dtype=tf.int32)], axis=-1), shape=(6, 8, 2)),},
                 'output': {
-                    'shape': (8, 6, 2),
+                    'shape': (2, 4, 3, 2, 2),
                     'values': tf.convert_to_tensor(
                         [
-                            [[0, 0], [1, 1], [8, 8], [9, 9], [16, 16], [17, 17]],
-                            [[2, 2], [3, 3], [10, 10], [11, 11], [18, 18], [19, 19]],
-                            [[4, 4], [5, 5], [12, 12], [13, 13], [20, 20], [21, 21]],
-                            [[6, 6], [7, 7], [14, 14], [15, 15], [22, 22], [23, 23]],
-                            [[24, 24], [25, 25], [32, 32], [33, 33], [40, 40], [41, 41]],
-                            [[26, 26], [27, 27], [34, 34], [35, 35], [42, 42], [43, 43]],
-                            [[28, 28], [29, 29], [36, 36], [37, 37], [44, 44], [45, 45]],
-                            [[30, 30], [31, 31], [38, 38], [39, 39], [46, 46], [47, 47]],])}},]
+                            [
+                                [[[0, 0], [1, 1]], [[8, 8], [9, 9]], [[16, 16], [17, 17]]],
+                                [[[2, 2], [3, 3]], [[10, 10], [11, 11]], [[18, 18], [19, 19]]],
+                                [[[4, 4], [5, 5]], [[12, 12], [13, 13]], [[20, 20], [21, 21]]],
+                                [[[6, 6], [7, 7]], [[14, 14], [15, 15]], [[22, 22], [23, 23]]],],
+                            [
+                                [[[24, 24], [25, 25]], [[32, 32], [33, 33]], [[40, 40], [41, 41]]],
+                                [[[26, 26], [27, 27]], [[34, 34], [35, 35]], [[42, 42], [43, 43]]],
+                                [[[28, 28], [29, 29]], [[36, 36], [37, 37]], [[44, 44], [45, 45]]],
+                                [[[30, 30], [31, 31]], [[38, 38], [39, 39]], [[46, 46], [47, 47]]],],])}},]
 
     def test_patching(self):
         for __case in self._test_cases:
