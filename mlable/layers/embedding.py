d11fe8bdf2df5ab8cf39ebdb7931f1fc3071e252
apehex <apehex@protonmail.com>
2025-01-22 14:37:12 +0100

    Base the other embedding layers on the extended generic class (for all ranks)

--- mlable/layers/embedding.py
+++ mlable/layers/embedding.py
@@ -167,6 +167,29 @@ class SinePositionalEmbedding(tf.keras.layers.Layer):
     def from_config(cls, config: dict) -> tf.keras.layers.Layer:
         return cls(**config)
 
+# GENERIC ######################################################################
+
+@tf.keras.utils.register_keras_serializable(package='layers')
+class GenericEmbedding(tf.keras.layers.Embedding):
+    def compute_internal_shape(self, input_shape: tuple) -> tuple:
+        return tuple(input_shape)[:1] + (math.prod(tuple(input_shape)[1:]),)
+
+    def compute_output_shape(self, input_shape: tuple) -> tuple:
+        return tuple(input_shape) + (self.output_dim,)
+
+    def build(self, input_shape: tuple) -> None:
+        super(GenericEmbedding, self).build(self.compute_internal_shape(input_shape))
+
+    def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
+        # this class lifts the limitation on tensor rank (<= 3) in the base class tf.keras.layers.Embedding
+        __shape = tuple(tf.shape(inputs))
+        # merge all the intermediate axes
+        __outputs = tf.reshape(inputs, shape=self.compute_internal_shape(__shape))
+        # embed each element separately
+        __outputs = super(GenericEmbedding, self).call(__outputs)
+        # split the intermediate axes back
+        return tf.reshape(__outputs, shape=self.compute_output_shape(__shape))
+
 # POSITION #####################################################################
 
 @tf.keras.utils.register_keras_serializable(package='layers')
@@ -186,7 +209,7 @@ class PositionalEmbedding(tf.keras.layers.Layer):
         __dim_s = __shape[__axis_s]
         __dim_f = __shape[__axis_f]
         # actually init & build
-        self._layer = tf.keras.layers.Embedding(input_dim=__dim_s, output_dim=__dim_f)
+        self._layer = GenericEmbedding(input_dim=__dim_s, output_dim=__dim_f)
         self._layer.build((__dim_s,))
         # register
         self.built = True
@@ -219,32 +242,9 @@ class PositionalEmbedding(tf.keras.layers.Layer):
 # TOKUN ########################################################################
 
 @tf.keras.utils.register_keras_serializable(package='layers')
-class TokunEmbedding(tf.keras.layers.Embedding):
+class TokunEmbedding(GenericEmbedding):
     def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
         # embed each element separately
         __outputs = super(TokunEmbedding, self).call(inputs)
         # concatenate the embeddings
         return mlable.shaping.merge(__outputs, left_axis=-2, right_axis=-1, left=True)
-
-# BASE #########################################################################
-
-@tf.keras.utils.register_keras_serializable(package='layers')
-class GenericEmbedding(tf.keras.layers.Embedding):
-    def compute_internal_shape(self, input_shape: tuple) -> tuple:
-        return tuple(input_shape)[:1] + (math.prod(tuple(input_shape)[1:]),)
-
-    def compute_output_shape(self, input_shape: tuple) -> tuple:
-        return tuple(input_shape) + (self.output_dim,)
-
-    def build(self, input_shape: tuple) -> None:
-        super(GenericEmbedding, self).build(self.compute_internal_shape(input_shape))
-
-    def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
-        # this class lifts the limitation on tensor rank (<= 3) in the base class tf.keras.layers.Embedding
-        __shape = tuple(tf.shape(inputs))
-        # merge all the intermediate axes
-        __outputs = tf.reshape(inputs, shape=self.compute_internal_shape(__shape))
-        # embed each element separately
-        __outputs = super(GenericEmbedding, self).call(__outputs)
-        # split the intermediate axes back
-        return tf.reshape(__outputs, shape=self.compute_output_shape(__shape))
