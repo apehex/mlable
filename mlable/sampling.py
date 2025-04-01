922bfe3f943ba69a0bee6fe0ac322569abb72d27
apehex <17317183+apehex@users.noreply.github.com>
2025-04-01 20:59:55 +0200

    Operate on logits rather than probabilities

--- mlable/sampling.py
+++ mlable/sampling.py
@@ -42,25 +42,25 @@ def filter_top_p(logits: tf.Tensor, threshold: tf.Tensor) -> tf.Tensor:
 
 # BINARY #######################################################################
 
-def binary(prediction: tf.Tensor, depth: int=-1, threshold: float=0.5, random: bool=False) -> tf.Tensor:
+def binary(logits: tf.Tensor, depth: int=-1, threshold: float=0.0, random: bool=False) -> tf.Tensor:
     # meta
-    __threshold = tf.cast(threshold, prediction.dtype)
+    __threshold = tf.cast(threshold, logits.dtype)
     # group the bits of each encoded value
-    __prediction = prediction if (depth < 2) else mlable.shaping.divide(prediction, input_axis=-2, output_axis=-1, factor=depth, insert=True)
+    __logits = logits if (depth < 2) else mlable.shaping.divide(logits, input_axis=-2, output_axis=-1, factor=depth, insert=True)
     # binary tensor
-    __bits = tf.cast(__prediction > __threshold, dtype=tf.int32)
+    __bits = tf.cast(__logits > __threshold, dtype=tf.int32)
     # expand to match the input rank
     return mlable.ops._reduce_base(data=__bits, base=2, axis=-1, keepdims=False)
 
 # CATEGORICAL #################################################################
 
-def categorical(prediction: tf.Tensor, depth: int=-1, random: bool=False) -> tf.Tensor:
+def categorical(logits: tf.Tensor, depth: int=-1, random: bool=False) -> tf.Tensor:
     # isolate each one-hot vector
-    __prediction = prediction if (depth < 2) else mlable.shaping.divide(prediction, input_axis=-2, output_axis=-1, factor=depth, insert=True)
+    __logits = logits if (depth < 2) else mlable.shaping.divide(logits, input_axis=-2, output_axis=-1, factor=depth, insert=True)
     # return the index with the highest probability
-    return tf.argmax(input=__prediction, axis=-1, output_type=tf.int32)
+    return tf.argmax(input=__logits, axis=-1, output_type=tf.int32)
 
 # RAW #########################################################################
 
-def raw(prediction: tf.Tensor, factor: float=256., dtype: tf.dtypes.DType=tf.int32, random: bool=False) -> tf.Tensor:
-    return tf.cast(tf.round(tf.cast(factor, prediction.dtype) * prediction), dtype)
+def raw(logits: tf.Tensor, factor: float=256., dtype: tf.dtypes.DType=tf.int32, random: bool=False) -> tf.Tensor:
+    return tf.cast(tf.round(tf.cast(factor, logits.dtype) * logits), dtype)
