189f3456b3203c7f4fca9a0118a08d5f6797a391
apehex <17317183+apehex@users.noreply.github.com>
2025-04-04 18:56:28 +0200

    Remove useless function

--- mlable/masking.py
+++ mlable/masking.py
@@ -9,11 +9,3 @@ def contrast(left: tf.Tensor, right: tf.Tensor, weight: float=1.0, dtype: tf.dty
     __mask = tf.cast(__mask, dtype=dtype)
     # highlight the differences
     return weight * __mask + (1.0 - weight) * (1.0 - __mask)
-
-# REPLACE ######################################################################
-
-def choose(left: tf.Tensor, right: tf.Tensor, mask: tf.Tensor) -> tf.Tensor:
-    # expand the right values to match the shape of the left
-    __right = tf.zeros_like(left) + tf.cast(right, dtype=left.dtype)
-    # choose the right values when mask is true, left otherwise
-    return tf.where(mask, x=__right, y=left)
