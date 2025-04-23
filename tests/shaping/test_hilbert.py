19da2424d2ed256d33e2ada88b94ef814d0c3c99
apehex <17317183+apehex@users.noreply.github.com>
2025-04-23 23:29:58 +0200

    Test each slice along the Hilbert curve

--- tests/shaping/test_hilbert.py
+++ tests/shaping/test_hilbert.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+import densecurves.hilbert
+import mlable.shapes
 import mlable.shaping.hilbert
 
 # FOLD #########################################################################
@@ -13,15 +15,15 @@ class FoldTest(tf.test.TestCase):
                     'data': tf.random.uniform((4, 4096, 3), minval=-1.0, maxval=1.0, dtype=tf.bfloat16),
                     'order': 4,
                     'rank': 3,
-                    'axis': 1,},
+                    'axis': -2,},
                 'shape': (4, 16, 16, 16, 3),},
             {
                 'args': {
-                    'data': tf.cast(tf.random.uniform((1, 2 ** (3 * 5), 1), minval=0, maxval=256, dtype=tf.int32), dtype=tf.uint8),
+                    'data': tf.cast(tf.random.uniform((1, 2 ** (3 * 3), 1), minval=0, maxval=256, dtype=tf.int32), dtype=tf.uint8),
                     'order': 3,
-                    'rank': 5,
+                    'rank': 3,
                     'axis': 1,},
-                'shape': (1, 8, 8, 8, 8, 8, 1),},]
+                'shape': (1, 8, 8, 8, 1),},]
 
     def test_shape_and_dtype(self):
         for __case in self._cases:
@@ -36,11 +38,33 @@ class FoldTest(tf.test.TestCase):
 
     def test_reciprocity(self):
         for __case in self._cases:
-            __args = {__k: __v for __k, __v in __case['args'].items() if __k in ['order', 'rank']}
-            __axes = range(__case['args']['axis'], __case['args']['axis'] + __case['args']['rank'])
+            __shape = list(__case['args']['data'].shape)
+            __order = __case['args']['order']
+            __rank = __case['args']['rank']
+            __axis = __case['args']['axis'] % len(__shape)
+            __axes = range(__axis, __axis + __rank)
             __folded = mlable.shaping.hilbert.fold(**__case['args'])
-            __back = mlable.shaping.hilbert.unfold(__folded, **__args, axes=__axes)
+            __back = mlable.shaping.hilbert.unfold(__folded, order=__order, rank=__rank, axes=__axes)
             self.assertAllEqual(__case['args']['data'], __back)
+
+    def test_each_value_along_the_curve(self):
+        for __case in self._cases:
+            __shape_flat = list(__case['args']['data'].shape)
+            __order = __case['args']['order']
+            __rank = __case['args']['rank']
+            __axis = __case['args']['axis'] % len(__shape_flat)
+            __axes = list(range(__axis, __axis + __rank))
+            __folded = mlable.shaping.hilbert.fold(**__case['args'])
+            __curve = [densecurves.hilbert.point(__i, order=__order, rank=__rank) for __i in range(2 ** (__order * __rank))]
+            for __i, __p in enumerate(__curve):
+                __shape_folded = list(__folded.shape)
+                __size_flat = [__d if (__j != __axis) else 1 for __j, __d in enumerate(__shape_flat)]
+                __size_folded = [__d if (__j not in __axes) else 1 for __j, __d in enumerate(__shape_folded)]
+                __begin_flat = [__i if (__j == __axis) else 0 for __j in range(len(__shape_flat))]
+                __begin_folded = [__p[__j - __axis] if (__j in __axes) else 0 for __j in range(len(__shape_folded))]
+                self.assertAllEqual(
+                    tf.squeeze(tf.slice(__case['args']['data'], __begin_flat, __size_flat)),
+                    tf.squeeze(tf.slice(__folded, __begin_folded, __size_folded)))
 
 # UNFOLD #######################################################################
 
