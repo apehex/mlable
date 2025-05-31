24b3724b60824f88f8d2bd9b8a61ea88c4ac4a7e
apehex <17317183+apehex@users.noreply.github.com>
2025-05-31 21:29:33 +0200

    Sort the convolution blocks + tidy up

--- mlable/blocks/convolution/resnet.py
+++ mlable/blocks/convolution/resnet.py
@@ -12,87 +12,6 @@ DROPOUT = 0.0
 EPSILON = 1e-6
 PADDING = 'same'
 
-# CONVOLUTION ##################################################################
-
-@tf.keras.utils.register_keras_serializable(package='blocks')
-class ConvolutionBlock(tf.keras.layers.Layer):
-    def __init__(self, channel_dim: int, kernel_dim: int, stride_dim: int, dropout_rate: float=DROPOUT, epsilon: float=EPSILON, padding: str=PADDING, **kwargs) -> None:
-        super(ConvolutionBlock, self).__init__(**kwargs)
-        # save config
-        self._config = {'channel_dim': channel_dim, 'kernel_dim': kernel_dim, 'stride_dim': stride_dim, 'dropout_rate': dropout_rate, 'epsilon': epsilon, 'padding': padding,}
-        # layers
-        self._layers = [
-            tf.keras.layers.LayerNormalization(axis=-1, epsilon=epsilon, center=True, scale=True),
-            tf.keras.layers.ReLU(),
-            tf.keras.layers.Dropout(rate=dropout_rate),
-            tf.keras.layers.Conv2D(filters=channel_dim, kernel_size=kernel_dim, padding=padding, strides=stride_dim, activation=None, use_bias=True, data_format='channels_last'),]
-
-    def build(self, input_shape: tuple) -> None:
-        # the input shape is progated / unchanged
-        for __l in self._layers:
-            __l.build(input_shape)
-        # register
-        self.built = True
-
-    def compute_output_shape(self, input_shape: tuple) -> tuple:
-        return functools.reduce(lambda __s, __l: __l.compute_output_shape(__s), self._layers, tuple(input_shape))
-
-    def call(self, inputs: tf.Tensor, training: bool=False, **kwargs) -> tf.Tensor:
-        return functools.reduce(lambda __t, __l: __l(__t, training=training, **kwargs), self._layers, inputs)
-
-    def get_config(self) -> dict:
-        __config = super(ConvolutionBlock, self).get_config()
-        __config.update(self._config)
-        return __config
-
-    @classmethod
-    def from_config(cls, config: dict) -> tf.keras.layers.Layer:
-        return cls(**config)
-
-# RESIDUAL #####################################################################
-
-@tf.keras.utils.register_keras_serializable(package='blocks')
-class ResidualBlock(ConvolutionBlock):
-    def call(self, inputs: tf.Tensor, training: bool=False, **kwargs) -> tf.Tensor:
-        return inputs + functools.reduce(lambda __t, __l: __l(__t, training=training, **kwargs), self._layers, inputs)
-
-# TRANSPOSE ####################################################################
-
-@tf.keras.utils.register_keras_serializable(package='blocks')
-class TransposeConvolutionBlock(tf.keras.layers.Layer):
-    def __init__(self, channel_dim: int, kernel_dim: int, stride_dim: int, dropout_rate: float=DROPOUT, epsilon: float=EPSILON, padding: str=PADDING, **kwargs) -> None:
-        super(TransposeConvolutionBlock, self).__init__(**kwargs)
-        # save config
-        self._config = {'channel_dim': channel_dim, 'kernel_dim': kernel_dim, 'stride_dim': stride_dim, 'dropout_rate': dropout_rate, 'epsilon': epsilon, 'padding': padding,}
-        # layers
-        self._layers = [
-            tf.keras.layers.LayerNormalization(axis=-1, epsilon=epsilon, center=True, scale=True),
-            tf.keras.layers.ReLU(),
-            tf.keras.layers.Dropout(rate=dropout_rate),
-            tf.keras.layers.Conv2DTranspose(filters=channel_dim, kernel_size=kernel_dim, padding=padding, strides=stride_dim, activation=None, use_bias=True, data_format='channels_last'),]
-
-    def build(self, input_shape: tuple) -> None:
-        # the input shape is progated / unchanged
-        for __l in self._layers:
-            __l.build(input_shape)
-        # register
-        self.built = True
-
-    def compute_output_shape(self, input_shape: tuple) -> tuple:
-        return functools.reduce(lambda __s, __l: __l.compute_output_shape(__s), self._layers, tuple(input_shape))
-
-    def call(self, inputs: tf.Tensor, training: bool=False, **kwargs) -> tf.Tensor:
-        return functools.reduce(lambda __t, __l: __l(__t, training=training, **kwargs), self._layers, inputs)
-
-    def get_config(self) -> dict:
-        __config = super(TransposeConvolutionBlock, self).get_config()
-        __config.update(self._config)
-        return __config
-
-    @classmethod
-    def from_config(cls, config: dict) -> tf.keras.layers.Layer:
-        return cls(**config)
-
 # RESNET #######################################################################
 
 @tf.keras.utils.register_keras_serializable(package='blocks')
@@ -101,8 +20,8 @@ class ResnetBlock(tf.keras.layers.Layer):
         self,
         channel_dim: int=None,
         group_dim: int=None,
-        dropout_rate: float=0.0,
-        epsilon_rate: float=1e-6,
+        dropout_rate: float=DROPOUT,
+        epsilon_rate: float=EPSILON,
         **kwargs
     ) -> None:
         super(ResnetBlock, self).__init__(**kwargs)
@@ -187,9 +106,9 @@ class EncoderBlock(tf.keras.layers.Layer):
         channel_dim: int,
         group_dim: int=32,
         layer_num: int=1,
-        dropout_rate: float=0.0,
-        epsilon_rate: float=1e-6,
         downsample_on: bool=True,
+        dropout_rate: float=DROPOUT,
+        epsilon_rate: float=EPSILON,
         **kwargs
     ) -> None:
         super(EncoderBlock, self).__init__(**kwargs)
@@ -198,10 +117,9 @@ class EncoderBlock(tf.keras.layers.Layer):
             'channel_dim': channel_dim,
             'group_dim': group_dim,
             'layer_num': layer_num,
-            'dropout_rate': dropout_rate,
-            'epsilon_rate': epsilon_rate,
             'downsample_on': downsample_on,
-        }
+            'dropout_rate': dropout_rate,
+            'epsilon_rate': epsilon_rate,}
         # layers
         self._blocks = []
 
@@ -256,8 +174,8 @@ class TransformerBlock(tf.keras.layers.Layer):
         head_dim: int=1,
         group_dim: int=32,
         layer_num: int=1,
-        dropout_rate: float=0.0,
-        epsilon_rate: float=1e-6,
+        dropout_rate: float=DROPOUT,
+        epsilon_rate: float=EPSILON,
         **kwargs
     ) -> None:
         super(TransformerBlock, self).__init__(**kwargs)
@@ -268,8 +186,7 @@ class TransformerBlock(tf.keras.layers.Layer):
             'group_dim': group_dim,
             'layer_num': layer_num,
             'dropout_rate': dropout_rate,
-            'epsilon_rate': epsilon_rate,
-        }
+            'epsilon_rate': epsilon_rate,}
         # layers
         self._merge_space = None
         self._split_space = None
@@ -342,9 +259,9 @@ class DecoderBlock(tf.keras.layers.Layer):
         channel_dim: int,
         group_dim: int=32,
         layer_num: int=1,
-        dropout_rate: float=0.0,
-        epsilon_rate: float=1e-6,
         upsample_on: bool=True,
+        dropout_rate: float=DROPOUT,
+        epsilon_rate: float=EPSILON,
         **kwargs
     ) -> None:
         super(DecoderBlock, self).__init__(**kwargs)
@@ -353,10 +270,9 @@ class DecoderBlock(tf.keras.layers.Layer):
             'channel_dim': channel_dim,
             'group_dim': group_dim,
             'layer_num': layer_num,
-            'dropout_rate': dropout_rate,
-            'epsilon_rate': epsilon_rate,
             'upsample_on': upsample_on,
-        }
+            'dropout_rate': dropout_rate,
+            'epsilon_rate': epsilon_rate,}
         # layers
         self._blocks = []
 
