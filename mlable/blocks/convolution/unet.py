111f29094c267564e32399d6564d57f207d995e1
apehex <apehex@protonmail.com>
2025-06-07 11:21:03 +0200

    Replace the group norm with an adaptive variant + factor the initialization

--- mlable/blocks/convolution/unet.py
+++ mlable/blocks/convolution/unet.py
@@ -4,6 +4,7 @@ import math
 import tensorflow as tf
 
 import mlable.blocks.convolution.resnet
+import mlable.blocks.normalization
 import mlable.layers.shaping
 
 # CONSTANTS ####################################################################
@@ -39,14 +40,14 @@ class AttentionBlock(tf.keras.layers.Layer):
         self._split_space = None
         self._attend_space = None
 
-    def build(self, input_shape: tuple) -> None:
-        __shape = tuple(input_shape)
+    def build(self, inputs_shape: tuple, contexts_shape: tuple=None) -> None:
+        __shape = tuple(inputs_shape)
         # fill the config with default values
         self._update_config(__shape)
         # factor args
         __norm_args = {'groups': self._config['group_dim'], 'epsilon': self._config['epsilon_rate'], 'axis': -1, 'center': True, 'scale': True,}
         # init layers
-        self._norm_channel = tf.keras.layers.GroupNormalization(**__norm_args)
+        self._norm_channel = mlable.blocks.normalization.AdaptiveGroupNormalization(**__norm_args)
         self._merge_space = mlable.layers.shaping.Merge(axis=1, right=True)
         self._split_space = mlable.layers.shaping.Divide(axis=1, factor=__shape[2], right=True, insert=True)
         self._attend_space = tf.keras.layers.MultiHeadAttention(
@@ -58,8 +59,8 @@ class AttentionBlock(tf.keras.layers.Layer):
             dropout=self._config['dropout_rate'],
             kernel_initializer='glorot_uniform')
         # build layers
-        self._norm_channel.build(__shape)
-        __shape = self._norm_channel.compute_output_shape(__shape)
+        self._norm_channel.build(__shape, contexts_shape=contexts_shape)
+        __shape = self._norm_channel.compute_output_shape(__shape, contexts_shape=contexts_shape)
         self._merge_space.build(__shape)
         __shape = self._merge_space.compute_output_shape(__shape)
         self._attend_space.build(query_shape=__shape, key_shape=__shape, value_shape=__shape)
@@ -69,12 +70,12 @@ class AttentionBlock(tf.keras.layers.Layer):
         # register
         self.built = True
 
-    def compute_output_shape(self, input_shape: tuple) -> tuple:
-        return tuple(input_shape)
+    def compute_output_shape(self, inputs_shape: tuple, contexts_shape: tuple=None) -> tuple:
+        return tuple(inputs_shape)
 
-    def call(self, inputs: tf.Tensor, training: bool=False, **kwargs) -> tf.Tensor:
+    def call(self, inputs: tf.Tensor, contexts: tf.Tensor=None, training: bool=False, **kwargs) -> tf.Tensor:
         # normalize the channels
-        __outputs = self._norm_channel(inputs, training=training)
+        __outputs = self._norm_channel(inputs, contexts=contexts, training=training)
         # merge the space axes
         __outputs = self._merge_space(__outputs)
         # attend to the space sequence
@@ -87,9 +88,9 @@ class AttentionBlock(tf.keras.layers.Layer):
         __config.update(self._config)
         return __config
 
-    def _update_config(self, input_shape: tuple) -> None:
+    def _update_config(self, inputs_shape: tuple) -> None:
         # parse the input shape
-        __shape = tuple(input_shape)
+        __shape = tuple(inputs_shape)
         __dim = int(__shape[-1])
         # fill with default values
         self._config['group_dim'] = self._config['group_dim'] or (2 ** int(0.5 * math.log2(__dim)))
@@ -131,81 +132,85 @@ class UnetBlock(tf.keras.layers.Layer):
             'add_upsampling': add_upsampling,
             'dropout_rate': max(0.0, dropout_rate),
             'epsilon_rate': max(1e-8, epsilon_rate),}
-        # layers
-        self._blocks = []
+        # blocks
+        self._resnet_blocks = []
+        self._sampling_blocks = []
 
-    def build(self, input_shape):
-        __shape = tuple(input_shape)
+    def build(self, inputs_shape: tuple, contexts_shape: tuple=None) -> None:
+        __shape = tuple(inputs_shape)
         # fill the config with default values
         self._update_config(__shape)
         # init the layers
         for _ in range(self._config['layer_num']):
             # always start with a resnet
-            self._blocks.append(mlable.blocks.convolution.resnet.ResnetBlock(
-                channel_dim=self._config['channel_dim'],
-                group_dim=self._config['group_dim'],
-                dropout_rate=self._config['dropout_rate'],
-                epsilon_rate=self._config['epsilon_rate']))
+            self._resnet_blocks.append(mlable.blocks.convolution.resnet.ResnetBlock(**self.get_resnet_config()))
             # interleave resnet and attention blocks
             if self._config['add_attention']:
-                self._blocks.append(AttentionBlock(
-                    group_dim=self._config['group_dim'],
-                    head_dim=self._config['head_dim'],
-                    head_num=self._config['head_num'],
-                    epsilon_rate=self._config['epsilon_rate'],
-                    dropout_rate=self._config['dropout_rate']))
+                self._resnet_blocks.append(AttentionBlock(**self.get_attention_config()))
         # postprocess the attention outputs with an extra resnet block
         if self._config['add_attention']:
-            self._blocks.append(mlable.blocks.convolution.resnet.ResnetBlock(
-                channel_dim=self._config['channel_dim'],
-                group_dim=self._config['group_dim'],
-                dropout_rate=self._config['dropout_rate'],
-                epsilon_rate=self._config['epsilon_rate']))
+            self._resnet_blocks.append(mlable.blocks.convolution.resnet.ResnetBlock(**self.get_resnet_config()))
         # add an optional downsampling block
         if self._config['add_downsampling']:
-            self._blocks.append(tf.keras.layers.Conv2D(
-                filters=self._config['channel_dim'],
-                kernel_size=3,
-                strides=2,
-                use_bias=True,
-                activation=None,
-                padding='same',
-                data_format='channels_last'))
+            self._sampling_blocks.append(tf.keras.layers.Conv2D(**self.get_convolution_config(strides=2)))
         # add an optional upsampling block
         if self._config['add_upsampling']:
-            self._blocks.append(tf.keras.layers.UpSampling2D(
-                size=(2, 2),
-                interpolation='nearest',
-                data_format='channels_last'))
-            self._blocks.append(tf.keras.layers.Conv2D(
-                filters=self._config['channel_dim'],
-                kernel_size=3,
-                strides=1,
-                use_bias=True,
-                activation=None,
-                padding='same',
-                data_format='channels_last'))
+            self._sampling_blocks.append(tf.keras.layers.UpSampling2D(**self.get_upsampling_config()))
+            self._sampling_blocks.append(tf.keras.layers.Conv2D(**self.get_convolution_config(strides=1)))
         # build
-        for __block in self._blocks:
+        for __block in self._resnet_blocks:
+            __block.build(__shape, contexts_shape=contexts_shape)
+            __shape = __block.compute_output_shape(__shape, contexts_shape=contexts_shape)
+        for __block in self._sampling_blocks:
             __block.build(__shape)
             __shape = __block.compute_output_shape(__shape)
         # register
         self.built = True
 
-    def call(self, inputs: tf.Tensor, training: bool=False, **kwargs) -> tf.Tensor:
-        return functools.reduce(lambda __x, __b: __b(__x, training=training, **kwargs), self._blocks, inputs)
+    def call(self, inputs: tf.Tensor, contexts: tf.Tensor=None, training: bool=False, **kwargs) -> tf.Tensor:
+        # succession of resnet and attention blocks
+        __outputs = functools.reduce(lambda __x, __b: __b(__x, contexts=contexts, training=training, **kwargs), self._resnet_blocks, inputs)
+        # either upsampling, downsampling or nothing
+        return functools.reduce(lambda __x, __b: __b(__x), self._sampling_blocks, __outputs)
 
-    def compute_output_shape(self, input_shape: tuple) -> tuple:
-        return functools.reduce(lambda __s, __b: __b.compute_output_shape(__s), self._blocks, input_shape)
+    def compute_output_shape(self, inputs_shape: tuple, contexts_shape: tuple=None) -> tuple:
+        # succession of resnet and attention blocks
+        __shape = functools.reduce(lambda __s, __b: __b.compute_output_shape(__s, contexts_shape=contexts_shape), self._resnet_blocks, inputs_shape)
+        # either upsampling, downsampling or nothing
+        return functools.reduce(lambda __s, __b: __b.compute_output_shape(__s), self._sampling_blocks, __shape)
 
     def get_config(self) -> dict:
         __config = super(UnetBlock, self).get_config()
         __config.update(self._config)
         return __config
 
-    def _update_config(self, input_shape: tuple) -> None:
+    def get_resnet_config(self) -> dict:
+        __keys = ['channel_dim', 'group_dim', 'dropout_rate', 'epsilon_rate']
+        return {__k: __v for __k, __v in self._config.items() if __k in __keys}
+
+    def get_attention_config(self) -> dict:
+        __keys = ['group_dim', 'head_dim', 'head_num', 'dropout_rate', 'epsilon_rate']
+        return {__k: __v for __k, __v in self._config.items() if __k in __keys}
+
+    def get_convolution_config(self, strides: int=1) -> dict:
+        return {
+            'filters': self._config['channel_dim'],
+            'strides': strides,
+            'kernel_size': 3,
+            'use_bias': True,
+            'activation': None,
+            'padding': 'same',
+            'data_format': 'channels_last',}
+
+    def get_upsampling_config(self) -> dict:
+        return {
+            'size': (2, 2),
+            'interpolation': 'bilinear',
+            'data_format': 'channels_last',}
+
+    def _update_config(self, inputs_shape: tuple) -> None:
         # parse the input shape
-        __shape = tuple(input_shape)
+        __shape = tuple(inputs_shape)
         __dim = int(__shape[-1])
         # fill with default values
         self._config['channel_dim'] = self._config['channel_dim'] or __dim
--- tests/blocks/convolution/test_unet.py
+++ tests/blocks/convolution/test_unet.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 import mlable.blocks.convolution.unet
 
-# BASE #########################################################################
+# GENERIC ######################################################################
 
 class UnetBlockTest(tf.test.TestCase):
     def setUp(self):
@@ -10,39 +10,47 @@ class UnetBlockTest(tf.test.TestCase):
         self._cases = [
             {
                 'inputs': tf.ones((2, 16, 16, 8), dtype=tf.float16),
+                'contexts': None,
                 'args': {'channel_dim': None, 'group_dim': None, 'head_dim': None, 'head_num': None, 'layer_num': None, 'add_attention': False, 'add_downsampling': False, 'add_upsampling': False, 'dropout_rate': 0.1, 'epsilon_rate': 1e-6,},
                 'outputs': {'shape': (2, 16, 16, 8),},},
             {
                 'inputs': tf.convert_to_tensor([16 * [16 * [8 * [__i]]] for __i in range(4)], dtype=tf.float32),
+                'contexts': tf.ones((4, 1, 1, 1), dtype=tf.float32),
                 'args': {'channel_dim': 32, 'group_dim': None, 'head_dim': None, 'head_num': None, 'layer_num': 1, 'add_attention': True, 'add_downsampling': False, 'add_upsampling': False, 'dropout_rate': 0.0, 'epsilon_rate': 1e-6,},
                 'outputs': {'shape': (4, 16, 16, 32),},},
             {
                 'inputs': tf.random.normal((2, 16, 16, 8), dtype=tf.float32),
+                'contexts': tf.random.normal((2, 1, 1, 4), dtype=tf.float32),
                 'args': {'channel_dim': 32, 'group_dim': None, 'head_dim': None, 'head_num': None, 'layer_num': 2, 'add_attention': True, 'add_downsampling': False, 'add_upsampling': False, 'dropout_rate': 0.0, 'epsilon_rate': 1e-6,},
                 'outputs': {'shape': (2, 16, 16, 32),},},
             {
                 'inputs': tf.random.normal((2, 16, 16, 8), dtype=tf.float32),
+                'contexts': tf.random.normal((2, 4), dtype=tf.float32),
                 'args': {'channel_dim': 32, 'group_dim': 8, 'head_dim': 8, 'head_num': 4, 'layer_num': 4, 'add_attention': False, 'add_downsampling': False, 'add_upsampling': False, 'dropout_rate': 0.0, 'epsilon_rate': 1e-6,},
                 'outputs': {'shape': (2, 16, 16, 32),},},
             {
                 'inputs': tf.random.normal((2, 16, 16, 8), dtype=tf.float32),
+                'contexts': tf.random.normal((2, 1, 1, 1), dtype=tf.float32),
                 'args': {'channel_dim': 32, 'group_dim': 8, 'head_dim': None, 'head_num': None, 'layer_num': 2, 'add_attention': False, 'add_downsampling': True, 'add_upsampling': False, 'dropout_rate': 0.0, 'epsilon_rate': 1e-6,},
                 'outputs': {'shape': (2, 8, 8, 32),},},
             {
                 'inputs': tf.random.normal((2, 16, 16, 8), dtype=tf.float32),
+                'contexts': tf.random.normal((2, 1, 1, 8), dtype=tf.float32),
                 'args': {'channel_dim': 4, 'group_dim': None, 'head_dim': None, 'head_num': None, 'layer_num': 2, 'add_attention': False, 'add_downsampling': False, 'add_upsampling': True, 'dropout_rate': 0.0, 'epsilon_rate': 1e-6,},
                 'outputs': {'shape': (2, 32, 32, 4),},},
             {
                 'inputs': tf.random.normal((2, 16, 16, 8), dtype=tf.float32),
+                'contexts': tf.random.normal((2, 1), dtype=tf.float32),
                 'args': {'channel_dim': 32, 'group_dim': None, 'head_dim': None, 'head_num': None, 'layer_num': 2, 'add_attention': True, 'add_downsampling': True, 'add_upsampling': False, 'dropout_rate': 0.0, 'epsilon_rate': 1e-6,},
                 'outputs': {'shape': (2, 8, 8, 32),},},
             {
                 'inputs': tf.random.normal((2, 16, 16, 8), dtype=tf.float32),
+                'contexts': tf.random.normal((2, 8), dtype=tf.float32),
                 'args': {'channel_dim': 4, 'group_dim': None, 'head_dim': None, 'head_num': None, 'layer_num': 2, 'add_attention': True, 'add_downsampling': False, 'add_upsampling': True, 'dropout_rate': 0.0, 'epsilon_rate': 1e-6,},
                 'outputs': {'shape': (2, 32, 32, 4),},},]
 
     def test_shape(self):
         for __case in self._cases:
             __layer = mlable.blocks.convolution.unet.UnetBlock(**__case['args'])
-            __outputs = __layer(__case['inputs'], training=False)
+            __outputs = __layer(__case['inputs'], contexts=__case['contexts'], training=False)
             self.assertEqual(tuple(__outputs.shape), __case['outputs']['shape'])
