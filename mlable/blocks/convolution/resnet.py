cfc241c70ebac0c6ae4e2160107e1bc4c165319f
apehex <apehex@protonmail.com>
2025-06-07 11:12:01 +0200

    Replace the group norm with an adaptive variant

--- mlable/blocks/convolution/resnet.py
+++ mlable/blocks/convolution/resnet.py
@@ -3,6 +3,8 @@ import math
 
 import tensorflow as tf
 
+import mlable.blocks.normalization
+
 # CONSTANTS ####################################################################
 
 DROPOUT = 0.0
@@ -36,53 +38,53 @@ class ResnetBlock(tf.keras.layers.Layer):
         self._drop = None
         self._silu = None
 
-    def build(self, input_shape: tuple) -> None:
-        __shape = tuple(input_shape)
+    def build(self, inputs_shape: tuple, contexts_shape: tuple=None) -> None:
+        __shape = tuple(inputs_shape)
         # parse
-        self._config['channel_dim'] = self._config['channel_dim'] or int(input_shape[-1])
-        self._config['group_dim'] = self._config['group_dim'] or (2 ** int(0.5 * math.log2(int(input_shape[-1]))))
+        self._config['channel_dim'] = self._config['channel_dim'] or int(inputs_shape[-1])
+        self._config['group_dim'] = self._config['group_dim'] or (2 ** int(0.5 * math.log2(int(inputs_shape[-1]))))
         # factor
         __norm_args = {'groups': self._config['group_dim'], 'epsilon': self._config['epsilon_rate'], 'center': True, 'scale': True,}
         __conv_args = {'filters': self._config['channel_dim'], 'use_bias': True, 'activation': None, 'padding': 'same', 'data_format': 'channels_last'}
         # init
-        self._norm1 = tf.keras.layers.GroupNormalization(**__norm_args)
-        self._norm2 = tf.keras.layers.GroupNormalization(**__norm_args)
+        self._norm1 = mlable.blocks.normalization.AdaptiveGroupNormalization(**__norm_args)
+        self._norm2 = mlable.blocks.normalization.AdaptiveGroupNormalization(**__norm_args)
         self._conv0 = tf.keras.layers.Conv2D(kernel_size=1, **__conv_args)
         self._conv1 = tf.keras.layers.Conv2D(kernel_size=3, **__conv_args)
         self._conv2 = tf.keras.layers.Conv2D(kernel_size=3, **__conv_args)
         self._drop = tf.keras.layers.Dropout(self._config['dropout_rate'])
         self._silu = tf.keras.activations.silu
         # build
-        self._norm1.build(__shape)
-        __shape = self._norm1.compute_output_shape(__shape)
+        self._norm1.build(__shape, contexts_shape=contexts_shape)
+        __shape = self._norm1.compute_output_shape(__shape, contexts_shape=contexts_shape)
         self._conv1.build(__shape)
         __shape = self._conv1.compute_output_shape(__shape)
-        self._norm2.build(__shape)
-        __shape = self._norm2.compute_output_shape(__shape)
+        self._norm2.build(__shape, contexts_shape=contexts_shape)
+        __shape = self._norm2.compute_output_shape(__shape, contexts_shape=contexts_shape)
         self._drop.build(__shape)
         __shape = self._drop.compute_output_shape(__shape)
         self._conv2.build(__shape)
         __shape = self._conv2.compute_output_shape(__shape)
-        self._conv0.build(input_shape)
+        self._conv0.build(inputs_shape)
         __shape = self._conv0.compute_output_shape(__shape)
         # register
         self.built = True
 
-    def call(self, inputs: tf.Tensor, training: bool=False, **kwargs) -> tf.Tensor:
+    def call(self, inputs: tf.Tensor, contexts: tf.Tensor=None, training: bool=False, **kwargs) -> tf.Tensor:
         # first branch
-        __outputs = self._norm1(inputs)
+        __outputs = self._norm1(inputs, contexts=contexts)
         __outputs = self._silu(__outputs)
         __outputs = self._conv1(__outputs)
         # second branch
-        __outputs = self._norm2(__outputs)
+        __outputs = self._norm2(__outputs, context=contexts)
         __outputs = self._silu(__outputs)
         __outputs = self._drop(__outputs, training=training)
         __outputs = self._conv2(__outputs)
         # add the residuals
         return __outputs + self._conv0(inputs)
 
-    def compute_output_shape(self, input_shape: tuple) -> tuple:
-        return tuple(input_shape)[:-1] + (self._config['channel_dim'],)
+    def compute_output_shape(self, inputs_shape: tuple, contexts_shape: tuple=None) -> tuple:
+        return tuple(inputs_shape)[:-1] + (self._config['channel_dim'],)
 
     def get_config(self) -> dict:
         __config = super(ResnetBlock, self).get_config()
--- tests/blocks/convolution/test_resnet.py
+++ tests/blocks/convolution/test_resnet.py
@@ -10,15 +10,17 @@ class ResnetBlockTest(tf.test.TestCase):
         self._cases = [
             {
                 'inputs': tf.ones((2, 16, 16, 8), dtype=tf.float16),
+                'contexts': tf.ones((2, 1, 1, 4), dtype=tf.float16),
                 'args': {'channel_dim': 8, 'group_dim': 2, 'dropout_rate': 0.1, 'epsilon_rate': 1e-6,},
                 'outputs': {'shape': (2, 16, 16, 8),},},
             {
                 'inputs': tf.convert_to_tensor([16 * [16 * [8 * [__i]]] for __i in range(4)], dtype=tf.float32),
+                'contexts': None,
                 'args': {'channel_dim': 16, 'group_dim': None, 'dropout_rate': 0.0, 'epsilon_rate': 1e-6,},
                 'outputs': {'shape': (4, 16, 16, 16),},},]
 
     def test_shape(self):
         for __case in self._cases:
             __layer = mlable.blocks.convolution.resnet.ResnetBlock(**__case['args'])
-            __outputs = __layer(__case['inputs'], training=False)
+            __outputs = __layer(__case['inputs'], contexts=__case['contexts'], training=False)
             self.assertEqual(tuple(__outputs.shape), __case['outputs']['shape'])
