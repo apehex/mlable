580772481ad6153d5064eb13af41f8d08a6e64cd
apehex <apehex@protonmail.com>
2025-06-10 18:48:12 +0200

    Factor the latent and normalized diffusion models

--- mlable/models/diffusion.py
+++ mlable/models/diffusion.py
@@ -32,7 +32,7 @@ def reduce_std(current: tf.Tensor, sample: tf.Tensor) -> tf.Tensor:
 # NORMALIZED DIFFUSION #########################################################
 
 @tf.keras.utils.register_keras_serializable(package='models')
-class NormalizedDiffusionModel(tf.keras.models.Model): # mlable.models.ContrastModel
+class BaseDiffusionModel(tf.keras.models.Model): # mlable.models.ContrastModel
     def __init__(
         self,
         start_rate: float=START_RATE, # signal rate at the start of the forward diffusion process
@@ -40,7 +40,7 @@ class NormalizedDiffusionModel(tf.keras.models.Model): # mlable.models.ContrastM
         **kwargs
     ) -> None:
         # init
-        super(NormalizedDiffusionModel, self).__init__(**kwargs)
+        super(BaseDiffusionModel, self).__init__(**kwargs)
         # save config for IO
         self._config = {'start_rate': start_rate, 'end_rate': end_rate,}
         # diffusion schedule
@@ -50,6 +50,24 @@ class NormalizedDiffusionModel(tf.keras.models.Model): # mlable.models.ContrastM
         self._std = tf.cast(1.0, dtype=self.compute_dtype)
         # save the data shape for generation
         self._shape = ()
+
+    # WEIGHTS ##################################################################
+
+    def build(self, input_shape: tuple) -> None:
+        self._shape = tuple(input_shape)
+
+    # SHAPES ###################################################################
+
+    def compute_output_shape(self, input_shape: tuple=(), batch_dim: int=0) -> tuple:
+        __shape = tuple(input_shape) or tuple(self._shape)
+        __batch_dim = int(batch_dim or __shape[0])
+        return (__batch_dim,) + __shape[1:]
+
+    def compute_rate_shape(self, input_shape: tuple=(), batch_dim: int=0) -> tuple:
+        __shape = BaseDiffusionModel.compute_output_shape(self, input_shape=input_shape, batch_dim=batch_dim)
+        return tuple(mlable.shapes.filter(__shape, axes=[0]))
+
+    # NORMALIZE ################################################################
 
     def _norm(self, data: tf.Tensor, dtype: tf.DType=None) -> tf.Tensor:
         __dtype = dtype or self.compute_dtype
@@ -74,28 +92,35 @@ class NormalizedDiffusionModel(tf.keras.models.Model): # mlable.models.ContrastM
         # compute the standard deviation
         self._std = __scale * __dataset.reduce(__cast(0.0), std_fn)
 
-    def build(self, input_shape: tuple) -> None:
-        self._shape = tuple(input_shape)
+    # END-TO-END PRE / POST PROCESSING #########################################
+
+    def preprocess(self, data: tf.Tensor, dtype: tf.DType=None) -> tf.Tensor:
+        # scale to N(0, I)
+        return self._norm(data, dtype=dtype)
 
     def postprocess(self, data: tf.Tensor) -> tf.Tensor:
-        # scale the pixel values back to the signal space
+        # scale back to the signal space
         __data = self._denorm(data)
         # enforce types
         return tf.cast(__data, dtype=tf.int32)
 
-    def denoise(self, noisy_data: tf.Tensor, noise_rates: tf.Tensor, signal_rates: tf.Tensor) -> tuple:
+    # NOISE ####################################################################
+
+    def denoise(self, noisy_data: tf.Tensor, noise_rates: tf.Tensor, data_rates: tf.Tensor, **kwargs) -> tuple:
         # predict noise component
-        __noises = self.call((noisy_data, noise_rates**2), training=False)
+        __noises = self.call((noisy_data, noise_rates), training=False, **kwargs)
         # remove noise component from data
-        __data = (noisy_data - noise_rates * __noises) / signal_rates
+        __data = (noisy_data - noise_rates * __noises) / data_rates
         # return both
         return __noises, __data
 
-    def reverse_diffusion(self, initial_noises: tf.Tensor, step_num: int, dtype: tf.DType=None) -> tf.Tensor:
+    # DIFFUSION ################################################################
+
+    def reverse_diffusion(self, initial_noises: tf.Tensor, step_num: int, dtype: tf.DType=None, **kwargs) -> tf.Tensor:
         __dtype = dtype or self.compute_dtype
         __cast = functools.partial(tf.cast, dtype=__dtype)
         # reverse diffusion = sampling
-        __shape = (int(initial_noises.shape[0]),) + (len(self._shape) - 1) * (1,)
+        __shape = BaseDiffusionModel.compute_rate_shape(self, input_shape=initial_noises.shape)
         __delta = __cast(1.0 / step_num)
         # the current predictions for the noise and the signal
         __noises = __cast(initial_noises)
@@ -107,57 +132,64 @@ class NormalizedDiffusionModel(tf.keras.models.Model): # mlable.models.ContrastM
             # remix the components, with a noise level corresponding to the current iteration
             __data = (__beta * __data + __alpha * __noises)
             # predict the cumulated noise in the sample, and remove it from the sample
-            __noises, __data = self.denoise(__data, __alpha, __beta)
+            __noises, __data = self.denoise(__data, __alpha, __beta, **kwargs)
         return __data
 
-    def generate(self, sample_num: int, step_num: int, dtype: tf.DType=None) -> tf.Tensor:
+    # SAMPLING #################################################################
+
+    def generate(self, sample_num: int, step_num: int, dtype: tf.DType=None, **kwargs) -> tf.Tensor:
         __dtype = dtype or self.compute_dtype
-        __cast = functools.partial(tf.cast, dtype=__dtype)
         # adapt the batch dimension
-        __shape = (sample_num,) + tuple(self._shape)[1:]
+        __shape = BaseDiffusionModel.compute_output_shape(self, batch_dim=sample_num)
         # sample the initial noise
         __noises = tf.random.normal(shape=__shape, dtype=__dtype)
         # remove the noise
-        __data = self.reverse_diffusion(__noises, step_num=step_num)
+        __data = self.reverse_diffusion(__noises, step_num=step_num, **kwargs)
         # denormalize
         return self.postprocess(__data)
 
+    # TRAINING #################################################################
+
     def train_step(self, data: tf.Tensor) -> dict:
         __dtype = self.compute_dtype
-        __shape_n = tuple(data.shape)
-        __shape_a = mlable.shapes.filter(data.shape, axes=[0])
         # normalize data to have standard deviation of 1, like the noises
-        __data = self._norm(data, dtype=__dtype)
+        __data = self.preprocess(data, dtype=__dtype)
+        # compute the shapes in the latent space
+        __shape_n = BaseDiffusionModel.compute_output_shape(self, input_shape=__data.shape)
+        __shape_a = BaseDiffusionModel.compute_rate_shape(self, input_shape=__data.shape)
         # sample the noises = targets
         __noises = tf.random.normal(shape=__shape_n, dtype=__dtype)
         # sample the diffusion angles
         __angles = tf.random.uniform(shape=__shape_a, minval=0.0, maxval=1.0, dtype=__dtype)
         # compute the signal to noise ratio
-        __noise_rates, __signal_rates = self._schedule(__angles, dtype=__dtype)
+        __noise_rates, __data_rates = self._schedule(__angles, dtype=__dtype)
         # mix the data with noises
-        __data = __signal_rates * __data + __noise_rates * __noises
+        __data = __data_rates * __data + __noise_rates * __noises
         # train to predict the noise from scrambled data
-        return super(NormalizedDiffusionModel, self).train_step(((__data, __noise_rates**2), __noises))
+        return super(BaseDiffusionModel, self).train_step(((__data, __noise_rates), __noises))
 
     def test_step(self, data: tf.Tensor) -> dict:
         __dtype = self.compute_dtype
-        __shape_n = tuple(data.shape)
-        __shape_a = mlable.shapes.filter(data.shape, axes=[0])
         # normalize data to have standard deviation of 1, like the noises
-        __data = self._norm(data, dtype=__dtype)
+        __data = self.preprocess(data, dtype=__dtype)
+        # compute the shapes in the latent space
+        __shape_n = BaseDiffusionModel.compute_output_shape(self, input_shape=__data.shape)
+        __shape_a = BaseDiffusionModel.compute_rate_shape(self, input_shape=__data.shape)
         # sample the noises = targets
         __noises = tf.random.normal(shape=__shape_n, dtype=__dtype)
         # sample the diffusion angles
         __angles = tf.random.uniform(shape=__shape_a, minval=0.0, maxval=1.0, dtype=__dtype)
         # compute the signal to noise ratio
-        __noise_rates, __signal_rates = self._schedule(__angles, dtype=__dtype)
+        __noise_rates, __data_rates = self._schedule(__angles, dtype=__dtype)
         # mix the data with noises
-        __data = __signal_rates * __data + __noise_rates * __noises
+        __data = __data_rates * __data + __noise_rates * __noises
         # train to predict the noise from scrambled data
-        return super(NormalizedDiffusionModel, self).test_step(((__data, __noise_rates**2), __noises))
+        return super(BaseDiffusionModel, self).test_step(((__data, __noise_rates), __noises))
+
+    # CONFIG ###################################################################
 
     def get_config(self) -> dict:
-        __config = super(NormalizedDiffusionModel, self).get_config()
+        __config = super(BaseDiffusionModel, self).get_config()
         __config.update(self._config)
         return __config
 
@@ -168,40 +200,16 @@ class NormalizedDiffusionModel(tf.keras.models.Model): # mlable.models.ContrastM
 # LATENT DIFFUSION #############################################################
 
 @tf.keras.utils.register_keras_serializable(package='models')
-class LatentDiffusionModel(tf.keras.models.Model): # mlable.models.ContrastModel
+class LatentDiffusionModel(BaseDiffusionModel): # mlable.models.ContrastModel
     def __init__(
         self,
         start_rate: float=START_RATE, # signal rate at the start of the forward diffusion process
         end_rate: float=END_RATE, # signal rate at the start of the forward diffusion process
         **kwargs
     ) -> None:
-        # init
-        super(LatentDiffusionModel, self).__init__(**kwargs)
-        # save config for IO
-        self._config = {'start_rate': start_rate, 'end_rate': end_rate,}
-        # diffusion schedule
-        self._schedule = functools.partial(mlable.schedules.cosine_rates, start_rate=start_rate, end_rate=end_rate)
-        # scale the data to a normal distribution and back
-        self._mean = tf.cast(0.0, dtype=self.compute_dtype)
-        self._std = tf.cast(1.0, dtype=self.compute_dtype)
-        # save the data shape for generation
-        self._shape = ()
+        super(LatentDiffusionModel, self).__init__(start_rate=start_rate, end_rate=end_rate, **kwargs)
         # encoding / decoding model
         self._vae = None
-
-    def build(self, input_shape: tuple) -> None:
-        self._shape = tuple(input_shape)
-
-    # SHAPES ###################################################################
-
-    def compute_output_shape(self, input_shape: tuple=(), batch_dim: int=0) -> tuple:
-        __shape = tuple(input_shape) or tuple(self._shape)
-        __batch_dim = int(batch_dim or __shape[0])
-        return (__batch_dim,) + __shape[1:]
-
-    def compute_variance_shape(self, input_shape: tuple=(), batch_dim: int=0) -> tuple:
-        __shape = self.compute_output_shape(input_shape=input_shape, batch_dim=batch_dim)
-        return tuple(mlable.shapes.filter(__shape, axes=[0]))
 
     # LATENT <=> SIGNAL SPACES #################################################
 
@@ -222,31 +230,6 @@ class LatentDiffusionModel(tf.keras.models.Model): # mlable.models.ContrastModel
         self._vae = model
         self._vae.trainable = trainable
 
-    # NORMALIZE ################################################################
-
-    def _norm(self, data: tf.Tensor, dtype: tf.DType=None) -> tf.Tensor:
-        __dtype = dtype or self.compute_dtype
-        __cast = functools.partial(tf.cast, dtype=__dtype)
-        return (__cast(data) - __cast(self._mean)) / __cast(self._std)
-
-    def _denorm(self, data: tf.Tensor, dtype: tf.DType=None) -> tf.Tensor:
-        __dtype = dtype or self.compute_dtype
-        __cast = functools.partial(tf.cast, dtype=__dtype)
-        return __cast(self._mean) + __cast(self._std) * __cast(data)
-
-    def adapt(self, dataset: tf.data.Dataset, dtype: tf.DType=None) -> None:
-        __dtype = dtype or self.compute_dtype
-        __cast = functools.partial(tf.cast, dtype=__dtype)
-        # compute the dataset cardinality
-        __scale = dataset.reduce(0, lambda __c, _: __c + 1)
-        __scale = __cast(1.0) / __cast(tf.maximum(1, __scale))
-        # compute the mean, in the latent space
-        self._mean = __scale * dataset.reduce(__cast(0.0), lambda __m, __x: __m + __cast(reduce_mean(self._encode(__x))))
-        self._mean = __cast(self._mean)
-        # compute the standard deviation, in the latent space
-        self._std = __scale * dataset.reduce(__cast(0.0), lambda __m, __x: __m + __cast(reduce_std(self._encode(__x))))
-        self._std = __cast(self._std)
-
     # PRE / POST PROCESSING ####################################################
 
     def preprocess(self, data: tf.Tensor, dtype: tf.DType=None) -> tf.Tensor:
@@ -260,87 +243,6 @@ class LatentDiffusionModel(tf.keras.models.Model): # mlable.models.ContrastModel
         __data = self._denorm(data, dtype=dtype)
         # decode back to the signal space
         return self._decode(__data, logits=logits, dtype=dtype)
-
-    # DENOISING ################################################################
-
-    def denoise(self, noisy_data: tf.Tensor, noise_rates: tf.Tensor, signal_rates: tf.Tensor) -> tuple:
-        # predict noise component
-        __noises = self.call((noisy_data, noise_rates**2), training=False)
-        # remove noise component from data
-        __data = (noisy_data - noise_rates * __noises) / signal_rates
-        # return both
-        return __noises, __data
-
-    def reverse_diffusion(self, initial_noises: tf.Tensor, step_num: int, dtype: tf.DType=None) -> tf.Tensor:
-        __dtype = dtype or self.compute_dtype
-        __cast = functools.partial(tf.cast, dtype=__dtype)
-        # reverse diffusion = sampling
-        __shape = self.compute_variance_shape(input_shape=initial_noises.shape)
-        __delta = __cast(1.0 / step_num)
-        # the current predictions for the noise and the signal
-        __noises = __cast(initial_noises)
-        __data = __cast(initial_noises)
-        for __i in range(step_num + 1):
-            # even pure noise (step 0) is considered to contain some signal
-            __angles = tf.ones(__shape, dtype=__dtype) - __cast(__i) * __delta
-            __alpha, __beta = self._schedule(__angles, dtype=__dtype)
-            # remix the components, with a noise level corresponding to the current iteration
-            __data = (__beta * __data + __alpha * __noises)
-            # predict the cumulated noise in the sample, and remove it from the sample
-            __noises, __data = self.denoise(__data, __alpha, __beta)
-        return __data
-
-    # SAMPLING #################################################################
-
-    def generate(self, sample_num: int, step_num: int, logits: bool=True, dtype: tf.DType=None) -> tf.Tensor:
-        __dtype = dtype or self.compute_dtype
-        __cast = functools.partial(tf.cast, dtype=__dtype)
-        # adapt the batch dimension
-        __shape = self.compute_output_shape(batch_dim=sample_num)
-        # sample the initial noise
-        __noises = tf.random.normal(shape=__shape, dtype=__dtype)
-        # remove the noise
-        __data = self.reverse_diffusion(__noises, step_num=step_num)
-        # denormalize and decode
-        return self.postprocess(__data, logits=logits, dtype=__dtype)
-
-    # TRAINING #################################################################
-
-    def train_step(self, data: tf.Tensor) -> dict:
-        __dtype = self.compute_dtype
-        # normalize data to have standard deviation of 1, like the noises
-        __data = self.preprocess(data, dtype=__dtype)
-        # compute the shapes in the latent space
-        __shape_n = self.compute_output_shape(input_shape=__data.shape)
-        __shape_a = self.compute_variance_shape(input_shape=__data.shape)
-        # sample the noises = targets
-        __noises = tf.random.normal(shape=__shape_n, dtype=__dtype)
-        # sample the diffusion angles
-        __angles = tf.random.uniform(shape=__shape_a, minval=0.0, maxval=1.0, dtype=__dtype)
-        # compute the signal to noise ratio
-        __noise_rates, __signal_rates = self._schedule(__angles, dtype=__dtype)
-        # mix the data with noises
-        __data = __signal_rates * __data + __noise_rates * __noises
-        # train to predict the noise from scrambled data
-        return super(LatentDiffusionModel, self).train_step(((__data, __noise_rates**2), __noises))
-
-    def test_step(self, data: tf.Tensor) -> dict:
-        __dtype = self.compute_dtype
-        # normalize data to have standard deviation of 1, like the noises
-        __data = self.preprocess(data, dtype=__dtype)
-        # compute the shapes in the latent space
-        __shape_n = self.compute_output_shape(input_shape=__data.shape)
-        __shape_a = self.compute_variance_shape(input_shape=__data.shape)
-        # sample the noises = targets
-        __noises = tf.random.normal(shape=__shape_n, dtype=__dtype)
-        # sample the diffusion angles
-        __angles = tf.random.uniform(shape=__shape_a, minval=0.0, maxval=1.0, dtype=__dtype)
-        # compute the signal to noise ratio
-        __noise_rates, __signal_rates = self._schedule(__angles, dtype=__dtype)
-        # mix the data with noises
-        __data = __signal_rates * __data + __noise_rates * __noises
-        # train to predict the noise from scrambled data
-        return super(LatentDiffusionModel, self).test_step(((__data, __noise_rates**2), __noises))
 
     # CONFIG ###################################################################
 
